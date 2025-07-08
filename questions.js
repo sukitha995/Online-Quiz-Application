f87e1895d3fcf32e09b75f1638c5d2df9a458d6d
Sukitha239 <sukitha.kothuru@gmail.com>
2025-07-08 14:26:42 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does the 'M' in MERN stack stand for?",
+        answer: "MongoDB",
+        options: [
+            "A. MySQL",
+            "B. MongoDB",
+            "C. Mongoose",
+            "D. Markdown",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which package is commonly used for routing in React?",
+        answer: "React Router",
+        options: [
+            "A. React Router",
+            "B. Express Router",
+            "C. Path Router",
+            "D. RouteJS",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which method is used to fetch data from MongoDB in Node.js using Mongoose?",
+        answer: "Model.find()",
+        options: [
+            "A. find()",
+            "B. getData()",
+            "C. Model.find()",
+            "D. fetch()",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which command starts a React development server?",
+        answer: "npm start",
+        options: [
+            "A. node start",
+            "B. npm start",
+            "C. npm serve",
+            "D. react-run",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following is a backend framework in the MERN stack?",
+        answer: "Express.js",
+        options: [
+            "A. React",
+            "B. Angular",
+            "C. Express.js",
+            "D. MongoDB",
+        ]
+    },
+    {
+        numb: 6,
+        question: "What is the purpose of useState in React?",
+        answer: "To manage state in functional components",
+        options: [
+            "A. To make API calls",
+            "B. To define classes",
+            "C. To manage state in functional components",
+            "D. To render components",
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which protocol is used for API communication in Express.js?",
+        answer: "HTTP",
+        options: [
+            "A. FTP",
+            "B. SSH",
+            "C. HTTP",
+            "D. SMTP",
+        ]
+    },
+    {
+        numb: 8,
+        question: "What does JSX stand for in React?",
+        answer: "JavaScript XML",
+        options: [
+            "A. JavaScript Xtreme",
+            "B. Java Syntax Extension",
+            "C. JavaScript Extension",
+            "D. JavaScript XML",
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which hook is used to perform side effects in React?",
+        answer: "useEffect",
+        options: [
+            "A. useState",
+            "B. useEffect",
+            "C. useContext",
+            "D. useReducer",
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which command is used to install Express in a Node.js project?",
+        answer: "npm install express",
+        options: [
+            "A. npm start express",
+            "B. npm add express",
+            "C. install express",
+            "D. npm install express",
+        ]
+    },
+];
